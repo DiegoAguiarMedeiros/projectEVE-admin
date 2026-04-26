ed257dac92d42cf502bf4ebc935a9b77abae4568
Diego Aguiar Medeiros <diego.a.medeiros@gmail.com>
2026-04-26 15:06:57 +0000

    Refactor Vite configuration to include environment variables and TypeScript checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,45 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import path from "path";
+import checker from "vite-plugin-checker";
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: { src: path.resolve(__dirname, './src') },
-  },
-  server: { port: 5174, host: '0.0.0.0' },
-})
+// ----------------------------------------------------------------------
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  const PORT = Number(process.env.PORT || env.PORT) || 3039;
+  const HOST = process.env.HOST || env.HOST || "0.0.0.0";
+
+  return {
+    plugins: [
+      react(),
+      checker({
+        typescript: true,
+        overlay: {
+          position: "tl",
+          initialIsOpen: false,
+        },
+      }),
+    ],
+    resolve: {
+      alias: [
+        {
+          find: /^~(.+)/,
+          replacement: path.join(process.cwd(), "node_modules/$1"),
+        },
+        {
+          find: /^src(.+)/,
+          replacement: path.join(process.cwd(), "src/$1"),
+        },
+      ],
+    },
+    server: { port: PORT, host: HOST, allowedHosts: "all" },
+    optimizeDeps: { force: true },
+    preview: {
+      port: PORT,
+      host: HOST,
+      allowedHosts: ["https://projecteve-admin.onrender.com"],
+    },
+  };
+});
